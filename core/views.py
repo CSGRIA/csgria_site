c90a476a2112c18e62cf49fcb40c5e6f000c3719
teach-genius <chantryolanda85@gmail.com>
2026-03-20 15:31:55 +0000

    v1

---
+++ core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+
+
+def home_view(request):
+    
+    return render(request,'core/home_page.html')
+
+
+def apropos_page(request):
+    return render(request,'core/apropos_page.html')
